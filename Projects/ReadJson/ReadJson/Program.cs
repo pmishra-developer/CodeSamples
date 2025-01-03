276fe15c3c7bf0740cebe9ea01ce7aef9dd58b1d
Pankaj Mishra <pankaj.mishra@sainsburys.co.uk>
2025-01-03 13:16:43 +0000

    Corrected Read Json Sample

--- Projects/ReadJson/ReadJson/Program.cs
+++ Projects/ReadJson/ReadJson/Program.cs
@@ -7,49 +7,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-        var fullFilePath = Path.Combine(desktop, "mfp_interface.json");
-        bool exists = File.Exists(fullFilePath);
+        // var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        // var fullFilePath = Path.Combine(desktop, "employee.json");
+        var filePath = "employee.json";
+        var exists = File.Exists(filePath);
         if (exists)
         {
-            var json = File.ReadAllText(fullFilePath);
-            var mfpInterface = JsonConvert.DeserializeObject<TableData>(json);
-            if (mfpInterface == null)
+            var json = File.ReadAllText(filePath);
+            var employees = JsonConvert.DeserializeObject<Employees>(json);
+            var test = employees;
+            Console.WriteLine("Serialisation Successful");
+            var count = employees.employees.Length;
+            Console.WriteLine($"{count} employees found");
+            employees.employees.ToList().ForEach(employee =>
             {
-                Console.WriteLine("Deserialization failed");
-                return;
-            }
-            
-            Console.WriteLine("Deserialization successful");
-            
-            Console.WriteLine("Landing Tables");
-            Console.WriteLine("-----------------");
-            var landingTableNames = mfpInterface.tableInformation.Select(x => x.LANDING_TABLE_NAME);
-            foreach (var landingTableName in landingTableNames)
-            {
-                if(!string.IsNullOrEmpty(landingTableName))
-                    Console.WriteLine(landingTableName);
-            }
-            
-            Console.WriteLine("                ");
-            Console.WriteLine("Staging Tables");
-            Console.WriteLine("-----------------");
-            var stagingTableNames = mfpInterface.tableInformation.Select(x => x.STAGING_TABLE_NAME);
-            foreach (var stagingTableName in stagingTableNames)
-            {
-                if (!string.IsNullOrEmpty(stagingTableName))
-                    Console.WriteLine(stagingTableName);
-            }
-
-            Console.WriteLine("                ");
-            Console.WriteLine("Destination Tables");
-            Console.WriteLine("-----------------");
-            var destinationTableNames = mfpInterface.tableInformation.Select(x => x.DESTINATION_TABLE_NAME);
-            foreach (var destinationTableName in destinationTableNames)
-            {
-                if (!string.IsNullOrEmpty(destinationTableName))
-                    Console.WriteLine(destinationTableName);
-            }
+                Console.WriteLine($"Name: {employee.firstName} {employee.lastName}");
+            });
         }
         else 
             Console.WriteLine("File does not exist");
