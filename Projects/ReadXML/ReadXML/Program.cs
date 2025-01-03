941bd03512f63f8e6a6f57d56af210013db60f90
Pankaj Mishra <pankaj.mishra@sainsburys.co.uk>
2025-01-03 13:52:12 +0000

    Minor Corrections

--- Projects/ReadXML/ReadXML/Program.cs
+++ Projects/ReadXML/ReadXML/Program.cs
@@ -7,27 +7,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        // var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-        // var fullFilePath = Path.Combine(desktop, "employee.json");
-        var filePath = "employee.xml";
-        var exists = File.Exists(filePath);
-        if (exists)
+        try
         {
-            var json = File.ReadAllText(filePath);
-            var serializer = new XmlSerializer(typeof(Employees));
-            var employees = (Employees)serializer.Deserialize(new StringReader(json))!;
-            
-            var test = employees;
-            Console.WriteLine("Serialisation Successful");
-            var count = employees.Employee.Count;
-            Console.WriteLine($"{count} employees found");
-            employees.Employee.ToList().ForEach(employee =>
+            Console.WriteLine("Hello, World!");
+            // var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            // var fullFilePath = Path.Combine(desktop, "employee.xml");
+            var filePath = "employee.xml";
+            var exists = File.Exists(filePath);
+            if (exists)
             {
-                Console.WriteLine($"Name: {employee.FirstName} {employee.LastName}");
-            });
+                var xmlText = File.ReadAllText(filePath);
+                var serializer = new XmlSerializer(typeof(Employees));
+                var employees = (Employees)serializer.Deserialize(new StringReader(xmlText))!;
+
+                Console.WriteLine("Serialisation Successful");
+                var count = employees.Employee.Count;
+                Console.WriteLine($"{count} employees found");
+                employees.Employee.ToList().ForEach(employee =>
+                {
+                    Console.WriteLine($"Name: {employee.FirstName} {employee.LastName}");
+                });
+            }
+            else
+                Console.WriteLine("File does not exist");
         }
-        else 
-            Console.WriteLine("File does not exist");
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 }
