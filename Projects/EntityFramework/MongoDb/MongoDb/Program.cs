cb9c1cd6f3e98fe5cff9ac4f9f6f4c2f7989bd3c
Pankaj Mishra <pankaj.mishra@sainsburys.co.uk>
2025-02-07 15:31:21 +0000

    Adding a Sample Usecase of Mongo Database with Ef

--- Projects/EntityFramework/MongoDb/MongoDb/Program.cs
+++ Projects/EntityFramework/MongoDb/MongoDb/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace MongoDb;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDb;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        
+        using(MongoDbContext context = new MongoDbContext())
+        {
+            context.Users.InsertOne(new User { name = "Pankaj Mishra" });
+            context.Users.InsertOne(new User { name = "Ashish Mishra" });
+            context.Users.InsertOne(new User { name = "Garima Mishra" });
+            context.Users.InsertOne(new User { name = "Sunidhi Mishra" });
+    
+            var users = context.Users.Find(new BsonDocument()).ToList();
+    
+            foreach(var user in users)
+            {
+                Console.WriteLine(user.name);
+            }
+        }
+
+        Console.ReadLine();
     }
 }
